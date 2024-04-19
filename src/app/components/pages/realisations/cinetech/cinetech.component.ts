d365b405b0dfad2ba36144b2dfa7fe9101c863ec
YohanDev27 <yoyodu27130@gmail.com>
2024-04-19 21:15:09 +0200

    Ajout des dernier projet

---
+++ src/app/components/pages/realisations/cinetech/cinetech.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cinetech',
+  templateUrl: './cinetech.component.html',
+  styleUrls: ['./cinetech.component.scss']
+})
+export class CinetechComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/pages/realisations/inov/inov.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inov',
+  templateUrl: './inov.component.html',
+  styleUrls: ['./inov.component.scss']
+})
+export class InovComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
